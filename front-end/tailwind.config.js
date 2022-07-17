2ca985b11c315601c12843a737801a17bb164573
Patrick Coshan <patrickcoshan@Patricks-MacBook-Pro.local>
2022-07-17 19:24:14 +0930

    font and tailiwnd init

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      studio : "#674AB3",
+      purpures: "#A348A6",
+      amethyst: "#A063C5",
+      medpurple: "#9174D8",
+      wisteria: "#CEA2D7",
+    },
+  },
+  plugins: [],
+}
