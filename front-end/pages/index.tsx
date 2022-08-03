6b68a7e4ced483e623c15b13851ce62ebd205a39
Patrick Coshan <patrickcoshan@Patricks-MacBook-Pro.local>
2022-08-03 12:56:55 +1000

    add file input

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
+  function handleFile() {
+    console.log('hello');
+  }
   return (
     <div className="min-h-screen h-full w-full relative bg-gradient-to-r from-studio via-purpures to-amethyst overflow-hidden">
       <div className="flex flex-col w-full items-center justify-center pt-32">
         <h1 className=" text-8xl animate-pulse px-4 ">VEXCHOWAVE</h1>
-        <div className="border-2 border-wisteria rounded-xl mt-10 text-center py-4 px-6 w-1/3 mx-auto cursor-pointer">
+        <label
+          className="border-2 border-wisteria rounded-xl mt-10 text-center py-4 px-6 w-1/3 mx-auto cursor-pointer"
+          onChange={handleFile}
+          htmlFor="trackinput"
+        >
           <p className="text-white lofi-font ">Upload your track here</p>
-        </div>
+          <input type="file" className="hidden" id="trackinput" />
+        </label>
       </div>
       <div className="">
         <img
